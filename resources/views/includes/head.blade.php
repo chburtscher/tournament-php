02fa32d250291c5eea63428c9e4afddd87b7e6b3
Christina Burtscher <christina.burtscher@gmx.at>
2017-01-31 19:10:39 +0100

    Fixed material icons

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,4 +1,5 @@
 <meta charset="UTF-8">
 <title>@yield('title')</title>
 <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
